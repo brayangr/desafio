eb21df1088e4162f69a278c80bdf93bb0018312f
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2017-12-14 00:54:56 -0300

    Se agrega simulacion del 10% de fallos en la consulta a la api en controlador

--- app/controllers/bienvenida_controller.rb
+++ app/controllers/bienvenida_controller.rb
@@ -1,20 +1,31 @@
 class BienvenidaController < ApplicationController
+
+	class ApiRequestFailed < StandardError; end
+
   def index
 		require 'net/http'
 		require 'json'
 		
+		#Se define la url del endpoint a consumir
 		url = ENV['API_URL'] + "api/cities"
-		p url
 		uri = URI(url)
-		response = Net::HTTP.get(uri)
-		response = JSON.parse(response)
-		p response
-		if cookies[:last_update].nil?
-			cookies[:last_update] = Time.now
+		begin
+			raise BienvenidaController::ApiRequestFailed, "How unfortunate! The API Request Failed" unless rand(0.0...1.0) > 0.1
+
+			response = Net::HTTP.get(uri)
+			response = JSON.parse(response)
+			
+			#Para cumplir el requisito de solo almacenar a la base de datos pasado de 10 segundos
+			#desde la ultima insercion, se define una cookie guardando la hora en que se realizo.
+			if cookies[:last_update].nil?
+				cookies[:last_update] = Time.now
+			end
+			if cookies[:last_update] < Time.now-10
+				cookies[:last_update] = Time.now
+			end
+		rescue BienvenidaController::ApiRequestFailed
+			retry
 		end
-		if cookies[:last_update] < Time.now-10
-			p cookies[:last_update] + " " + Time.now.to_s
-			cookies[:last_update] = Time.now
-		end
+		
   end
 end
