60a93a7390c13ed29edfc27ac80c4d8ade018826
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2017-12-14 00:24:34 -0300

    Se agregan variables de entorno para setear direccion de la API y se consume la api en el controlador

--- app/controllers/bienvenida_controller.rb
+++ app/controllers/bienvenida_controller.rb
@@ -1,2 +1,20 @@
 class BienvenidaController < ApplicationController
+  def index
+		require 'net/http'
+		require 'json'
+		
+		url = ENV['API_URL'] + "api/cities"
+		p url
+		uri = URI(url)
+		response = Net::HTTP.get(uri)
+		response = JSON.parse(response)
+		p response
+		if cookies[:last_update].nil?
+			cookies[:last_update] = Time.now
+		end
+		if cookies[:last_update] < Time.now-10
+			p cookies[:last_update] + " " + Time.now.to_s
+			cookies[:last_update] = Time.now
+		end
+  end
 end
