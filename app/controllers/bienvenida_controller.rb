52266c9532f5264e5d06f04727360d7b2ad57b04
Brayan Gonzalez <brayan.a.gonzalez@gmail.com>
2017-12-14 01:55:27 -0300

    Se muestran los elementos obtenidos desde la api en la vista

--- app/controllers/bienvenida_controller.rb
+++ app/controllers/bienvenida_controller.rb
@@ -6,15 +6,18 @@ class BienvenidaController < ApplicationController
 		require 'net/http'
 		require 'json'
 		
+		@temperatures = Array.new
+
 		#Se define la url del endpoint a consumir
 		url = ENV['API_URL'] + "api/cities"
 		uri = URI(url)
 		begin
+			#simula un 10% de posibilidades de fallar la consulta a la api
 			raise BienvenidaController::ApiRequestFailed, "How unfortunate! The API Request Failed" unless rand(0.0...1.0) > 0.1
 
 			response = Net::HTTP.get(uri)
 			response = JSON.parse(response)
-			
+			p response
 			#Para cumplir el requisito de solo almacenar a la base de datos pasado de 10 segundos
 			#desde la ultima insercion, se define una cookie guardando la hora en que se realizo.
 			if cookies[:last_update].nil?
@@ -23,9 +26,13 @@ class BienvenidaController < ApplicationController
 			if cookies[:last_update] < Time.now-10
 				cookies[:last_update] = Time.now
 			end
+
+			response.each do |r|
+				@temperatures.append(CitiesTemperature.new(r))
+			end
+			p @temperatures
 		rescue BienvenidaController::ApiRequestFailed
 			retry
 		end
-		
   end
 end
---
+++ app/models/cities_temperature.rb
@@ -0,0 +1,2 @@
+class CitiesTemperature < ApplicationRecord
+end
